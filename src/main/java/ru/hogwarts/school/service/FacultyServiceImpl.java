d3bda3153b2fb57445943e1476df9d5612443c1d
Timur-Gus <timurka24478@gmail.ru>
2024-04-06 16:57:53 +0300

    Домашняя работа 3.5

--- src/main/java/ru/hogwarts/school/service/FacultyServiceImpl.java
+++ src/main/java/ru/hogwarts/school/service/FacultyServiceImpl.java
@@ -1,14 +1,11 @@
 package ru.hogwarts.school.service;
 
-import ch.qos.logback.core.filter.Filter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.model.Faculty;
 import ru.hogwarts.school.model.Student;
 import ru.hogwarts.school.repository.FacultyRepository;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class FacultyServiceImpl implements FacultyService {
--- src/test/java/ru/hogwarts/school/constants/TestConstants.java
+++ src/test/java/ru/hogwarts/school/constants/TestConstants.java
@@ -1,4 +1,22 @@
 package ru.hogwarts.school.constants;
 
+import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.model.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class TestConstants {
+    public static final Student STUDENT_1 = new Student(1L, "Студент 1", 14);
+    public static final Student STUDENT_2 = new Student(2L, "Студент 2", 15);
+    public static final Student STUDENT_3 = new Student(3L, "Студент 3", 14);
+    public static final Faculty FACULTY_1 = new Faculty(1L, "Факультет 1", "Красный");
+    public static final Faculty FACULTY_2 = new Faculty(2L, "Факультет 2", "Синий");
+    public static final Faculty FACULTY_3 = new Faculty(3L, "Факультет 3", "Красный");
+    public static final List<Student> STUDENTS_BY_AGE = new ArrayList<>(List.of(
+            STUDENT_1,STUDENT_3
+    ));
+    public static final List<Faculty> FACULTIES_BY_AGE = new ArrayList<>(List.of(
+            FACULTY_1,FACULTY_3
+    ));
 }
