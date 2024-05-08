204cab0c82ca62158df5fc9fb5d23999b63a0d14
Timur-Gus <timurka24478@gmail.ru>
2024-05-08 10:59:32 +0300

    Убрал лишнее

--- src/main/java/ru/hogwarts/school/controller/InfoController.java
+++ src/main/java/ru/hogwarts/school/controller/InfoController.java
@@ -24,22 +24,10 @@ public class InfoController {
 
     @GetMapping("/sum")
     public Integer getSum() {
-        Long start1 = System.currentTimeMillis();
-        int sum = Stream
-                .iterate(1, a -> a +1)
-                .limit(1_000_000)
-                .reduce(0, (a, b) -> a + b );
-        Long end1 = System.currentTimeMillis();
-        System.out.println(end1-start1);
-
-        Long start2 = System.currentTimeMillis();
-        Integer sum2 = IntStream
-                .iterate(1, a -> a + 1)
+        Integer sum = IntStream
+                .iterate(1, a -> a < 1_000_000, a -> a + 1)
                 .parallel()
-                .limit(1_000_000)
                 .sum();
-        Long end2 = System.currentTimeMillis();
-        System.out.println(end2-start2);
-        return sum2;
+        return sum;
     }
 }
